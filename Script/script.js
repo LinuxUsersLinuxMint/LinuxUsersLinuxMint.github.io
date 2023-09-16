15c454ce5a3c959ba2db8d7d23cab711a8019876
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-09-16 18:49:44 +0300

    Update script.js

--- Script/script.js
+++ Script/script.js
@@ -1,2 +1,2 @@
-alert("Geliştirme Aşaması 5 - Güncellemesine Hoşgeldiniz Bir sonraki Güncellemenin geleceği Tarih 16 Eylül , 2023 , 23:20")
-alert("Welcome to Development Phase 5 - Update The next Update will arrive on September 16 , 2023 , 23:20")
+alert("Geliştirme Aşaması 5 - Güncellemesine Hoşgeldiniz Bir sonraki Güncellemenin geleceği Tarih 16 Eylül , 2023 , 00:00")
+alert("Welcome to Development Phase 5 - Update The next Update will arrive on September 16 , 2023 , 00:00")
