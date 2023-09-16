22e500ea390de4d26c25a45d7f0c447d6d5f5f75
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-09-16 23:46:27 +0300

    Add files via upload

--- Script/script.js
+++ Script/script.js
@@ -1,2 +1,2 @@
-alert("Geliştirme Aşaması 5 - Güncellemesine Hoşgeldiniz Bir sonraki Güncellemenin geleceği Tarih 16 Eylül , 2023 , 00:00")
-alert("Welcome to Development Phase 5 - Update The next Update will arrive on September 16 , 2023 , 00:00")
+alert("Geliştirme Aşaması 6 - Güncellemesine Hoşgeldiniz Bir sonraki Güncellemenin geleceği Tarih XX XXXXX , XXXX , XX:XX")
+alert("Welcome to Development Phase 6 - Update The next Update will arrive on XXXXX XX , XXXX , XX:XX")
