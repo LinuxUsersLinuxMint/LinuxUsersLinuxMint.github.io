f84e6c89ccc85052d925ff8de424fab7b5af02df
LinuxUsersLinuxMint <umutarda863@gmail.com>
2023-09-23 14:29:04 +0300

    Initial commit

--- Script/lang.js
+++ Script/lang.js
@@ -1,7 +1,7 @@
 var lang =  navigator.language || navigator.userLanguage
 if (lang == "tr-TR" | lang == "tr") {
-    document.location.href = "/TR/"
+    document.location.href = "/TR/index.html"
 }
 else {
-    document.location.href = "/EN/";
+    document.location.href = "/EN/index.html";
 }
