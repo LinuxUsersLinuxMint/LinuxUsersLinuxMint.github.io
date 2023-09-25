c8e98ac772153238b86a1e80c481a79da24c27d4
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-09-25 18:16:12 +0300

    Update script_en.js

--- Script/script_en.js
+++ Script/script_en.js
@@ -1,1 +1,2 @@
 alert("Welcome to Development Phase 6 - Update The next Update will arrive on XXXXX XX , XXXX , XX:XX")
+alert("On 09/27/2023, Wednesday, September 27, Time: 17:30-22:30, the Website will be temporarily closed due to maintenance (Review-Inspection-Detecting-Solving Problems).")
