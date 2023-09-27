49503e0d6add1d49ef4f4e8dae95321b33400a67
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-09-27 17:22:45 +0300

    Update maintenance.js

--- Script/maintenance.js
+++ Script/maintenance.js
@@ -1,1 +1,2 @@
-
+alert("BAKIM BİLDİRİMİ: 09/27/2023 - 10/01/2023 27 Eylül Çarşamba Saat: 17:30 - 01 Ekim Pazar Saat: 22:30  Tarihleri arası Web Sitesi Bakım Sebebiyle Kapatılmıştır.")
+alert("MAINTENANCE NOTICE: The Website has been closed due to maintenance between 09/27/2023 - 10/01/2023, Wednesday, September 27, Time: 17:30 - Sunday, October 01, Time: 22:30.")
