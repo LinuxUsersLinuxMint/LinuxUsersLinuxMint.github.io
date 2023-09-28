30cc18a10b8b855f38c1e8009c6e11b9573a0bc9
LinuxUsersLinuxMint <umutarda863@gmail.com>
2023-09-28 22:31:29 +0300

    Initial commit

---
+++ PY_FILES/maintenance_1.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+display("The Website has been closed due to maintenance between 09/27/2023 - 10/01/2023, Wednesday, September 27, Time: 17:30 - Sunday, October 01, Time: 22:30.")
