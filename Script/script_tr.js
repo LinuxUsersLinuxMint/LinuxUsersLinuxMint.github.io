70155a29eef4024294ef820eaaf3eedb376c8afe
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-09-30 00:05:45 +0300

    Update script_tr.js

--- Script/script_tr.js
+++ Script/script_tr.js
@@ -1,2 +1,1 @@
 alert("Geliştirme Aşaması 6 - Güncellemesine Hoşgeldiniz Bir sonraki Güncellemenin geleceği Tarih XX XXXXX , XXXX , XX:XX")
-alert("09/27/2023 27 Eylül Çarşamba Saat: 17:30-22:30 Tarihlerinde Web Sitesi Geçici olarak bakım(Gözden Geçirme-İnceleme-Sorunları Algılama-Çözme) sebebiyle kapatılıcaktır.")
