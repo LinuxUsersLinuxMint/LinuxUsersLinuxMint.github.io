a544da0e0d93d3b24fa0b822d654ff121b6219c5
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-10-06 22:42:36 +0300

    Update script_en.js

--- Script/script_en.js
+++ Script/script_en.js
@@ -1,1 +1,1 @@
-alert("Welcome to Development Phase 6 - Update The next Update will arrive on XXXXX XX , XXXX , XX:XX")
+// alert("Welcome to Development Phase 6 - Update The next Update will arrive on XXXXX XX , XXXX , XX:XX")
