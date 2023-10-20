6b1591c5b4c137724d0d724862a88b06761dbf50
LinuxUsersLinuxMint <umutarda863@gmail.com>
2023-10-20 23:43:53 +0300

    Initial commit

--- Script/lang.js
+++ Script/lang.js
@@ -1,9 +1,13 @@
 var lang =  navigator.language || navigator.userLanguage;
-if (lang == "tr-TR" | lang == "tr") 
+if (lang == "tr-TR" | lang == "tr" | lang == "tr_TR") 
 {
-    document.location.href = "/TR/index.html"
+    document.location.href = "/TR/index.html";
 }
-if (lang == "en-EN" | lang == "en" | lang == "en-US")
+if (lang == "en-EN" | lang == "en" | lang == "en_US" | lang == "en-US")
 {
     document.location.href = "/EN/index.html";
+}
+else
+{
+    alert("You are outside the TR or EN region. Support for languages of other countries has not been introduced.");
 }
