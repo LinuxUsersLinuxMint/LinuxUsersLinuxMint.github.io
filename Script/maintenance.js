2ac943f3aa584fa68c103271412b79c369964fe1
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-10-22 15:10:01 +0300

    .css files in /EN/ and /TR/ have been moved to /CSS/**/style.css.

--- Script/maintenance.js
+++
@@ -1,2 +0,0 @@
-alert("BAKIM BİLDİRİMİ: 10/08/2023 - 10/13/2023 08 Ekim Pazar Saat: 17:30 - 13 Ekim Pazar Saat: 22:30  Tarihleri arası Web Sitesi Bakım Sebebiyle Kapatılmıştır.")
-alert("MAINTENANCE NOTICE: The Website has been closed due to maintenance between 10/08/2023 - 10/13/2023, Sunday, October 08, Time: 17:30 - Sunday, October 13, Time: 22:30.")
