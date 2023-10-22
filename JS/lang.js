08672c58ee4ad302b9c7ff7b96a1a00880fcf7e2
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-10-22 14:36:21 +0300

    Add files via upload

---
+++ JS/lang.js
@@ -0,0 +1,13 @@
+var lang =  navigator.language || navigator.userLanguage;
+if (lang == "tr-TR" | lang == "tr" | lang == "tr_TR") 
+{
+    document.location.href = "/TR/index.html";
+}
+else if (lang == "en-EN" | lang == "en" | lang == "en_US" | lang == "en-US")
+{
+    document.location.href = "/EN/index.html";
+}
+else if (lang != "en-EN" | lang != "en" | lang != "en_US" | lang != "en-US" | lang != "tr-TR" | lang != "tr" | lang != "tr_TR")
+{
+    alert("You are outside the TR or EN region. Support for languages of other countries has not been introduced.");
+}
---
+++ JS/maintenanceend.js
@@ -0,0 +1,2 @@
+// alert("BAKIM SONU BİLDİRİMİ: 09/27/2023 - 10/01/2023 27 Eylül Çarşamba Saat: 17:30 - 01 Ekim Pazar Saat: 22:30  Tarihleri arası Olan Bakım 09/30/2023 20:59 Tarihinde Kapatılmıştır. (01 Ekim de kaldırılıcaktır)","Bakım Sonu Bildirimi")
+// alert("END OF MAINTENANCE NOTIFICATION: The maintenance between 09/27/2023 - 10/01/2023, Wednesday, September 27, Time: 17:30 - Sunday, October 01, Time: 22:30, was closed on 09/30/2023, 20:59. (It will be removed on October 01)","Ends Of Maintenance Notification")
