299d2a74216f25d6124044c0478aae4b64520812
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-10-26 18:24:20 +0300

    Update lang.js

--- JS/lang.js
+++ JS/lang.js
@@ -7,6 +7,11 @@ else if (lang == "en-EN" | lang == "en" | lang == "en_US" | lang == "en-US")
 {
     document.location.href = "/EN/index.html";
 }
+else if (lang == "ıl-IL" | lang == "ıl" | lang == "ıl_IL" | lang == "ıl-IL")
+{
+    document.location.href = "/index.html";
+    // עד שהמלחמה הזו תיפסק. בתור LinuxUsersLinuxMint, הוחלט שלא לשרת את המדינה שנקראת "ישראל". (לייצר תוכנה, לא מלחמה.)
+}
 else if (lang != "en-EN" | lang != "en" | lang != "en_US" | lang != "en-US" | lang != "tr-TR" | lang != "tr" | lang != "tr_TR")
 {
     alert("You are outside the TR or EN region. Support for languages of other countries has not been introduced.");
