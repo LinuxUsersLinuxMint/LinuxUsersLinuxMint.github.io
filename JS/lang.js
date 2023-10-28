7fafdf5709f9e7e3e18a1103783cd25034a4154d
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-10-28 18:49:47 +0300

    An important security vulnerability has been fixed.

--- JS/lang.js
+++ JS/lang.js
@@ -10,6 +10,7 @@ else if (lang == "en-EN" | lang == "en" | lang == "en_US" | lang == "en-US")
 else if (lang != "en-EN" | lang != "en" | lang != "en_US" | lang != "en-US" | lang != "tr-TR" | lang != "tr" | lang != "tr_TR")
 {
     alert("You are outside the TR or EN region. Support for languages of other countries has not been introduced.");
+    document.location.href = "/index.html";
 }
 while (lang == "ıl-IL" | lang == "ıl" | lang == "ıl_IL" | lang == "ıl-IL")
 {
