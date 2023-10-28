caffcf5f47674d3f99133950a947c1a024b27883
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-10-28 17:07:18 +0300

    Increase security for the website.

---
+++ JS/lang_tr.js
@@ -0,0 +1,19 @@
+var lang =  navigator.language || navigator.userLanguage;
+if (lang == "tr-TR" | lang == "tr" | lang == "tr_TR") 
+{
+    console.log("[linuxuserslinuxmint.github.io] USER_LOCATION=tr_TR");
+}
+else if (lang == "en-EN" | lang == "en" | lang == "en_US" | lang == "en-US")
+{
+    document.location.href = "/EN/index.html";
+}
+else if (lang != "tr-TR" | lang != "tr" | lang != "tr_TR")
+{
+    alert("You are outside the TR or EN region. Support for languages of other countries has not been introduced.");
+}
+while (lang == "ıl-IL" | lang == "ıl" | lang == "ıl_IL" | lang == "ıl-IL")
+{
+    document.location.href = "/index.html"; // האתר בלולאה והופך ללא פעיל.
+    // עקב המלחמות הנוכחיות, הוחלט כי LinuxUsersLinuxMint לא תספק שירות באופן קבוע למדינה הנקראת "ישראל". (צור תוכנה, אל תילחם.)
+    // החלטה זו היא קבועה ולא תשונה.
+}
