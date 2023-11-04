03618a16ea69b8d465d52affec0b3567ee89789d
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-11-05 00:15:57 +0300

    A number of problems have been corrected.

--- JS/lang.js
+++ JS/lang.js
@@ -14,7 +14,7 @@ else if (lang == "en-EN" | lang == "en" | lang == "en_US" | lang == "en-US")
 else {
     console.log("[linuxuserslinuxmint.github.io]: You are outside the TR or EN region. Support for languages of other countries has not been introduced.");
 }
-while (lang == "ıl_IL" | lang == "ıl" | lang == "ıl-IL" | lang == "il-İL" | lang == "il" | lang == "il_İL")
+while (lang == "ıl_IL" | lang == "ıl" | lang == "ıl-IL" | lang == "il-İL" | lang == "il" | lang == "il_İL" | lang == "o")
 {
     document.location.href = "/index.html"; // האתר בלולאה והופך ללא פעיל.
     // עקב המלחמות הנוכחיות, הוחלט כי LinuxUsersLinuxMint לא תספק שירות באופן קבוע למדינה הנקראת "ישראל". (צור תוכנה, אל תילחם.)
