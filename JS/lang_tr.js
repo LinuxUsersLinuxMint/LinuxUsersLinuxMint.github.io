45daf01d6f0d188913b6c91983ac625ba9f7fd99
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-11-10 17:54:32 +0300

    Temporarily disabling the security system.

--- JS/lang_tr.js
+++ JS/lang_tr.js
@@ -1,20 +1,20 @@
-var lang =  navigator.language || navigator.userLanguage;
-if (lang == "tr-TR" | lang == "tr" | lang == "tr_TR") 
-{
-    console.log("[linuxuserslinuxmint.github.io] USER_LOCATION=tr_TR");
-}
-else if (lang == "en-EN" | lang == "en" | lang == "en_US" | lang == "en-US")
-{
-    document.location.href = "/EN/index.html";
-}
-else if (lang != "tr-TR" | lang != "tr" | lang != "tr_TR")
-{
-    alert("You are outside the TR or EN region. Support for languages of other countries has not been introduced.");
-    document.location.href = "/index.html";
-}
-while (lang == "ıl-IL" | lang == "ıl" | lang == "ıl_IL" | lang == "ıl-IL")
-{
-    document.location.href = "/index.html"; // האתר בלולאה והופך ללא פעיל.
+//var lang =  navigator.language || navigator.userLanguage;
+//if (lang == "tr-TR" | lang == "tr" | lang == "tr_TR") 
+//{
+//    console.log("[linuxuserslinuxmint.github.io] USER_LOCATION=tr_TR");
+//}
+//else if (lang == "en-EN" | lang == "en" | lang == "en_US" | lang == "en-US")
+//{
+//    document.location.href = "/EN/index.html";
+//}
+//else if (lang != "tr-TR" | lang != "tr" | lang != "tr_TR")
+//{
+//    alert("You are outside the TR or EN region. Support for languages of other countries has not been introduced.");
+//    document.location.href = "/index.html";
+//}
+//while (lang == "ıl-IL" | lang == "ıl" | lang == "ıl_IL" | lang == "ıl-IL")
+//{
+//    document.location.href = "/index.html"; // האתר בלולאה והופך ללא פעיל.
     // עקב המלחמות הנוכחיות, הוחלט כי LinuxUsersLinuxMint לא תספק שירות באופן קבוע למדינה הנקראת "ישראל". (צור תוכנה, אל תילחם.)
     // החלטה זו היא קבועה ולא תשונה.
-}
+//}
