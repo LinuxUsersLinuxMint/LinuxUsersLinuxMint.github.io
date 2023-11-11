523696b0c72fd9e2617246f05c5d1a369f2c6fa1
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-11-11 15:09:39 +0300

    Unnecessary/unused files have been deleted. (Python will no longer be available on the website.)

--- PY_FILES/maintenance.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/python3
-display("10/08/2023 - 10/13/2023 08 Ekim Pazar Saat: 17:30 - 13 Ekim Pazar Saat: 22:30  Tarihleri arası Web Sitesi Bakım Sebebiyle Kapatılmıştır.")
--- PY_FILES/maintenance_1.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/python3
-display("The Website has been closed due to maintenance between 10/08/2023 - 10/13/2023, Sunday, October 08, Time: 17:30 - Sunday, October 13, Time: 22:30.")
