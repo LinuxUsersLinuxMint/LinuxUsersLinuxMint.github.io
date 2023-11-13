04b3a0814b1cd9fe18b7c3189d8ba1dd0ab7634f
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-11-13 23:54:08 +0300

    The website has been temporarily closed.
    Signed-off-by: Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>

--- JS/lang_tr.js
+++ JS/lang_tr.js
@@ -1,17 +1,17 @@
-//var lang =  navigator.language || navigator.userLanguage;
-//if (lang == "tr-TR" | lang == "tr" | lang == "tr_TR") 
-//{
-//    console.log("[linuxuserslinuxmint.github.io] USER_LOCATION=tr_TR");
-//}
-//else if (lang == "en-EN" | lang == "en" | lang == "en_US" | lang == "en-US")
-//{
-//    document.location.href = "/EN/index.html";
-//}
-//else if (lang != "tr-TR" | lang != "tr" | lang != "tr_TR")
-//{
-//    alert("You are outside the TR or EN region. Support for languages of other countries has not been introduced.");
-//    document.location.href = "/index.html";
-//}
+var lang =  navigator.language || navigator.userLanguage;
+if (lang == "tr-TR" | lang == "tr" | lang == "tr_TR") 
+{
+    document.location.href = "/index.html";
+}
+else if (lang == "en-EN" | lang == "en" | lang == "en_US" | lang == "en-US")
+{
+    document.location.href = "/index.html";
+}
+else if (lang != "tr-TR" | lang != "tr" | lang != "tr_TR")
+{
+    alert("You are outside the TR or EN region. Support for languages of other countries has not been introduced.");
+    document.location.href = "/index.html";
+}
 //while (lang == "ıl-IL" | lang == "ıl" | lang == "ıl_IL" | lang == "ıl-IL")
 //{
 //    document.location.href = "/index.html"; // האתר בלולאה והופך ללא פעיל.
