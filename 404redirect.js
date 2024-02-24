fa3c99a7fe86e12635ced636fdc07ce74aa94797
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2024-02-24 13:53:19 +0000

    bug fix.

---
+++ 404redirect.js
@@ -0,0 +1,9 @@
+var lang = navigator.language || navigator.userLanguage;
+if (lang == "tr-TR" | lang == "tr" | lang == "tr_TR")
+{
+    document.location.href = "/404NotFound/404.html";
+}
+else if (lang == "en-EN" | lang == "en" | lang == "en_US" | lang == "en-US") 
+{
+    document.location.href = "/404NotFound/404.html";
+}
