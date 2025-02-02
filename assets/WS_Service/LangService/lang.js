ef0b87df0a4e1ac0eb9ef5fa8ae0123b2aceb8f3
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2025-02-02 23:12:40 +0300

    The change has been withdrawn.

--- assets/WS_Service/LangService/lang.js
+++ assets/WS_Service/LangService/lang.js
@@ -1,11 +1,11 @@
 var lang = navigator.language;
 if (lang.startsWith("tr"))
 {
-    document.location.href = "/WS_Pages/TR/";
+    document.location.href = "/WS_Pages/TR/mainpage.html";
 }
 else if (lang.startsWith("en"))
 {
-    document.location.href = "/WS_Pages/EN/";
+    document.location.href = "/WS_Pages/EN/mainpage.html";
 }
 else if (lang != "en-EN" | lang != "en" | lang != "en_US" | lang != "en-US" | lang != "tr-TR" | lang != "tr" | lang != "tr_TR")
 {
