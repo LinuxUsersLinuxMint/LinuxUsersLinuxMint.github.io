fb296d0649d4ba998dbba3a49b3156e16e05723e
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2025-02-02 23:26:12 +0300

    Link redirects in the language service and 404 redirect service have also been changed.

--- assets/WS_Service/404redirect/404redirect.js
+++ assets/WS_Service/404redirect/404redirect.js
@@ -1,9 +1,9 @@
 var lang = navigator.language;
 if (lang.startsWith("tr"))
 {
-    document.location.href = "/404NotFound/404.html";
+    document.location.href = "/404NotFound/404";
 }
 else if (lang.startsWith("en")) 
 {
-    document.location.href = "/404NotFound/404.html";
+    document.location.href = "/404NotFound/404";
 }
--- assets/WS_Service/LangService/lang.js
+++ assets/WS_Service/LangService/lang.js
@@ -1,11 +1,11 @@
 var lang = navigator.language;
 if (lang.startsWith("tr"))
 {
-    document.location.href = "/WS_Pages/TR/mainpage.html";
+    document.location.href = "/WS_Pages/TR/mainpage";
 }
 else if (lang.startsWith("en"))
 {
-    document.location.href = "/WS_Pages/EN/mainpage.html";
+    document.location.href = "/WS_Pages/EN/mainpage";
 }
 else if (lang != "en-EN" | lang != "en" | lang != "en_US" | lang != "en-US" | lang != "tr-TR" | lang != "tr" | lang != "tr_TR")
 {
