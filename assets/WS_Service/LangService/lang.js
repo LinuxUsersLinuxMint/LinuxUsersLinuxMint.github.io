890fe871b20aca3b6bd7be7aa5371d3f03e5b369
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2025-02-02 17:25:55 +0300

    A small improvement has been made to the language service.

--- assets/WS_Service/LangService/lang.js
+++ assets/WS_Service/LangService/lang.js
@@ -10,7 +10,5 @@ else if (lang.startsWith("en"))
 else if (lang != "en-EN" | lang != "en" | lang != "en_US" | lang != "en-US" | lang != "tr-TR" | lang != "tr" | lang != "tr_TR")
 {
     alert("You are outside the TR or EN region. Support for languages of other countries has not been introduced.");
-}
-else {
     console.log("[linuxuserslinuxmint.github.io]: You are outside the TR or EN region. Support for languages of other countries has not been introduced.");
 }
