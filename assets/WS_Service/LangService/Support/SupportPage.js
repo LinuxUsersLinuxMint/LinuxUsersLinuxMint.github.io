e8257a116524a4aae9e06dfd9cac0f77d3ff3f5e
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2025-03-09 22:01:39 +0300

    The new language system for the support page is now available.

---
+++ assets/WS_Service/LangService/Support/SupportPage.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    var lang = navigator.language || navigator.userLanguage;
+    var changelocation = document.getElementById("changelocation");
+    var lxlmdoc = document.getElementById("Updates");
+
+    if (lang.startsWith("en")) {
+        changelocation.innerHTML = "Change location for the website";
+        lxlmdoc.innerHTML = "Documentation for LinuxUsersLinuxMint software";
+        document.title = "LinuxUsersLinuxMint - Support Page";
+    }
+    else if (lang.startsWith("tr")) {
+        changelocation.innerHTML = "Web sitesi için konum değiştirme";
+        lxlmdoc.innerHTML = "LinuxUsersLinuxMint yazılımları için dokümanlar";
+        document.title = "LinuxUsersLinuxMint - Destek Sayfası"
+    }
+};
