bb9e878e4410bafac9d6ec6e098e0df72f49112d
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2025-04-04 18:07:31 +0300

    A notification for a content on the support page.

---
+++ assets/WS_Service/location.js
@@ -0,0 +1,10 @@
+var lang = navigator.language;
+
+if (lang.startsWith("tr"))
+{
+    alert("Bu sayfa yakında yeni dil sisteminin gelmesi sebebiyle kullanımdan kaldırılacaktır ve arşive alınacaktır.");
+}
+else if (lang.startsWith("en"))
+{
+    alert("This page will soon be retired due to the new language system and will be archived.");
+}
