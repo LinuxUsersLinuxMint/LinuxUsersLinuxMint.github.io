33941c2a3a9a2947962f62dcb08fa017dc57c9ff
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2025-04-04 18:11:47 +0300

    Minor edit.

--- assets/WS_Service/LangService/Support/SupportPage.js
+++ assets/WS_Service/LangService/Support/SupportPage.js
@@ -4,12 +4,12 @@ window.onload = function() {
     var lxlmdoc = document.getElementById("lxlmdoc");
 
     if (lang.startsWith("en")) {
-        changelocation.innerHTML = "Change location for the website";
+        changelocation.innerHTML = "Change location for the website (Will be retired very soon)";
         lxlmdoc.innerHTML = "Documentation for LinuxUsersLinuxMint software";
         document.title = "LinuxUsersLinuxMint - Support Page";
     }
     else if (lang.startsWith("tr")) {
-        changelocation.innerHTML = "Web sitesi için konum değiştirme";
+        changelocation.innerHTML = "Web sitesi için konum değiştirme (Çok yakında kullanımdan kaldırılacaktır)";
         lxlmdoc.innerHTML = "LinuxUsersLinuxMint yazılımları için dokümanlar";
         document.title = "LinuxUsersLinuxMint - Destek Sayfası"
     }
