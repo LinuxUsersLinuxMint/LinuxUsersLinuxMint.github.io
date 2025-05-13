a06400f6d3905effa17461c6b1dec8c964655f42
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2025-05-13 23:08:33 +0300

    Improvement to the language checker service (lang.js).

--- assets/WS_Service/LangService/lang.js
+++ assets/WS_Service/LangService/lang.js
@@ -7,7 +7,7 @@ else if (lang.startsWith("en"))
 {
     document.location.href = "/WS_Pages/EN/mainpage";
 }
-else if (lang != "en-EN" | lang != "en" | lang != "en_US" | lang != "en-US" | lang != "tr-TR" | lang != "tr" | lang != "tr_TR")
+else if (!lang.startsWith("en") && !lang.startsWith("tr"))
 {
     alert("You are outside the TR or EN region. Support for languages of other countries has not been introduced.");
     console.log("[linuxuserslinuxmint.github.io]: You are outside the TR or EN region. Support for languages of other countries has not been introduced.");
