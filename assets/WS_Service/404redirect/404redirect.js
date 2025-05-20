7331de2b36ccead3d490765406d545c2db3fc1b9
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2025-05-20 18:51:24 +0300

    404.html and 404redirect.js files were removed because the file path of the 404 error page was changed.

--- assets/WS_Service/404redirect/404redirect.js
+++
@@ -1,9 +0,0 @@
-var lang = navigator.language;
-if (lang.startsWith("tr"))
-{
-    document.location.href = "/404NotFound/404";
-}
-else if (lang.startsWith("en")) 
-{
-    document.location.href = "/404NotFound/404";
-}
